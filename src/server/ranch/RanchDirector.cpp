1404e80e8a62e2410ea0b2cb7c6fbe5f428bfb6f
Heather <hjanggeog9@gmail.com>
2025-10-16 01:15:30 +0900

    Add real-time inventory update for breeding failure cards
    NOTE: There's a UI bug that displays red card's reward instead of yellow card when the user gets a yellow card reward. But the actual items they receive are the yellow card awards.

--- src/server/ranch/RanchDirector.cpp
+++ src/server/ranch/RanchDirector.cpp
@@ -573,9 +573,33 @@ void RanchDirector::SendStorageNotification(
 
 void RanchDirector::SendInventoryUpdate(ClientId clientId)
 {
-  // TODO: Find the proper way to trigger client-side inventory refresh
-  // Currently items only show up after client restart
-  // The client auto-refreshes carrots but not items in real-time
+  const auto& clientContext = GetClientContext(clientId);
+  const auto characterRecord = GetServerInstance().GetDataDirector().GetCharacter(
+    clientContext.characterUid);
+
+  if (not characterRecord)
+    return;
+
+  protocol::AcCmdCRGetItemFromStorageOK response{
+    .storageItemUid = 0,
+    .items = {},
+    .updatedCarrots = 0};
+
+  characterRecord.Immutable(
+    [this, &response](const data::Character& character)
+    {
+      const auto itemRecords = GetServerInstance().GetDataDirector().GetItemCache().Get(
+        character.inventory());
+      protocol::BuildProtocolItems(response.items, *itemRecords);
+      response.updatedCarrots = character.carrots();
+    });
+
+  _commandServer.QueueCommand<decltype(response)>(
+    clientId,
+    [response]()
+    {
+      return response;
+    });
 }
 
 void RanchDirector::BroadcastChangeAgeNotify(
@@ -1691,7 +1715,7 @@ void RanchDirector::HandleBreedingFailureCardChoose(
       response.item.uid = item.uid();
       response.item.tid = item.tid();
       response.item.expiresAt = 0;
-      response.item.count = rewardData->itemCount;
+      response.item.count = item.count();
     });
   } else {
     const auto newItem = GetServerInstance().GetDataDirector().CreateItem();
